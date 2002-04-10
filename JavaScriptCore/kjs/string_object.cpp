e6e6c2da453fc4ccbf67f270ea2b6ac995c18f24
darin <darin@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2002-04-10 21:16:22 +0000

    KDE 3.0 final versions of some files.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/kde@996 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/string_object.cpp
+++ JavaScriptCore/kjs/string_object.cpp
@@ -204,7 +204,7 @@ Value StringProtoFuncImp::call(ExecState *exec, Object &thisObj, const List &arg
     }
     else
     { /*
-       *  ECMA 15.5.4.12 String.prototype.search (regexp) 
+       *  ECMA 15.5.4.12 String.prototype.search (regexp)
        *  If regexp is not an object whose [[Class]] property is "RegExp", it is
        *  replaced with the result of the expression new RegExp(regexp).
        */
@@ -236,16 +236,34 @@ Value StringProtoFuncImp::call(ExecState *exec, Object &thisObj, const List &arg
         global = true;
 
       RegExpObjectImp* regExpObj = static_cast<RegExpObjectImp*>(exec->interpreter()->builtinRegExp().imp());
-      int **ovector = regExpObj->registerRegexp( reg, u );
       int lastIndex = 0;
       u3 = a1.toString(exec); // replacement string
       // This is either a loop (if global is set) or a one-way (if not).
       do {
+        int **ovector = regExpObj->registerRegexp( reg, u );
         UString mstr = reg->match(u, lastIndex, &pos, ovector);
         len = mstr.size();
-        lastIndex = pos + u3.size();
+        UString rstr(u3);
+        bool ok;
+        // check if u3 matches $1 or $2 etc
+        for (int i = 0; (i = rstr.find(UString("$"), i)) != -1; i++) {
+          if (i+1<rstr.size() && rstr[i+1] == '$') {  // "$$" -> "$"
+            rstr = rstr.substr(0,i) + "$" + rstr.substr(i+2);
+            continue;
+          }
+          // Assume number part is one char exactly
+          unsigned long pos = rstr.substr(i+1,1).toULong(&ok);
+          if (ok && pos <= (unsigned)reg->subPatterns()) {
+            rstr = rstr.substr(0,i)
+                      + u.substr((*ovector)[2*pos],
+                                     (*ovector)[2*pos+1]-(*ovector)[2*pos])
+                      + rstr.substr(i+2);
+            i += (*ovector)[2*pos+1]-(*ovector)[2*pos] - 1; // -1 offsets i++
+          }
+        }
+        lastIndex = pos + rstr.size();
         if ( pos != -1 )
-          u = u.substr(0, pos) + u3 + u.substr(pos + len);
+          u = u.substr(0, pos) + rstr + u.substr(pos + len);
         //fprintf(stderr,"pos=%d,len=%d,lastIndex=%d,u=%s\n",pos,len,lastIndex,u.ascii());
       } while ( global && pos != -1 );
 
