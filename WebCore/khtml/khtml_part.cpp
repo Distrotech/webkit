17ef1aea31027fd96ad8843540f7692cab59949c
mjs <mjs@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2003-01-14 21:23:55 +0000

    Reviewed by Darin.
    	- fixed 3144771 - Crash in KJS::WindowFunc::tryCall leaving www.smogcheck.org
    
    	I fixed this by making sure we couldn't send the unload event twice, which was
    	happening before in certain circumstances.
    
            * khtml/khtml_part.cpp:
            (KHTMLPart::closeURL): When emitting load event, note that unload
    	event has not yet been sent.
            (KHTMLPart::checkEmitLoadEvent): Note when unload event has been
    	sent to avoid sending it twice.
            * khtml/khtmlpart_p.h:
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Alexander-48-branch@3320 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/khtml_part.cpp
+++ WebCore/khtml/khtml_part.cpp
@@ -501,11 +501,11 @@ bool KHTMLPart::closeURL()
   if ( d->m_doc && d->m_doc->isHTMLDocument() ) {
     HTMLDocumentImpl* hdoc = static_cast<HTMLDocumentImpl*>( d->m_doc );
 
-    if ( hdoc->body() && d->m_bLoadEventEmitted ) {
+    if ( hdoc->body() && d->m_bLoadEventEmitted && !d->m_bUnloadEventEmitted ) {
       hdoc->body()->dispatchWindowEvent( EventImpl::UNLOAD_EVENT, false, false );
       if ( d->m_doc )
         d->m_doc->updateRendering();
-      d->m_bLoadEventEmitted = false;
+      d->m_bUnloadEventEmitted = true;
     }
   }
 
@@ -1718,6 +1718,7 @@ void KHTMLPart::checkEmitLoadEvent()
   }
 
   d->m_bLoadEventEmitted = true;
+  d->m_bUnloadEventEmitted = false;
   if (d->m_doc)
     d->m_doc->close();
 }
