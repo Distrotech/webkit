d673425b32be368de5aa421aa8f1b7cc6777a526
(no author) <(no author)@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2005-03-28 23:21:25 +0000

    This commit was manufactured by cvs2svn to create tag 'WebCore-415'.
    git-svn-id: http://svn.webkit.org/repository/webkit/tags/WebCore-415@8984 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Misc.subproj/WebKitSystemBits.h
+++
@@ -1,9 +0,0 @@
-/*	
-    WebKitSystemBits.h
-    Copyright (C) 2003 Apple Computer, Inc. All rights reserved.    
-*/
-
-#import <Foundation/Foundation.h>
-
-vm_size_t WebSystemMainMemory(void);
-int WebNumberOfCPUs(void);
