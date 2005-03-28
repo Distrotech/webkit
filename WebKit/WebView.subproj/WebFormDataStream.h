7ab38a93432c30b0f7311797be9147b321db506a
(no author) <(no author)@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2005-03-28 02:15:42 +0000

    This commit was manufactured by cvs2svn to create tag 'WebCore-413'.
    git-svn-id: http://svn.webkit.org/repository/webkit/tags/WebCore-413@8981 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/WebFormDataStream.h
+++
@@ -1,9 +0,0 @@
-/*
-    WebFormDataStream.h
-    Copyright 2005 Apple Computer, Inc.
-*/
-
-@class NSArray;
-@class NSMutableURLRequest;
-
-void webSetHTTPBody(NSMutableURLRequest *request, NSArray *formData);
