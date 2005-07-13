49582fb5638b4170a168527a80ff9c835467a3d6
adele <adele@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2005-07-13 23:46:15 +0000

    Reviewed by Maciej.
            * khtml/ecma/kjs_window.cpp: (WindowFunc::tryCall): removed isnan calls and used IS_NAN macro instead to get this to build on Panther.
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Safari-1-3-branch@9745 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/ecma/kjs_window.cpp
+++ WebCore/khtml/ecma/kjs_window.cpp
@@ -68,12 +68,6 @@
 
 #define IS_NAN(d) (!(d == d))
 
-// Must include <cmath> instead of <math.h> because of a bug in the
-// gcc 3.3 library version of <math.h> where if you include both
-// <cmath> and <math.h> the macros necessary for functions like
-// isnan are not defined.
-#include <cmath>
-
 using DOM::DocumentImpl;
 using DOM::DOMString;
 using DOM::ElementImpl;
@@ -86,8 +80,6 @@ using KParts::URLArgs;
 using KParts::WindowArgs;
 
 using namespace KJS;
-
-using std::isnan;
 
 namespace KJS {
 
@@ -1723,7 +1715,7 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
             if (key == "left" || key == "screenx") {
               bool ok;
               double d = val.toDouble(&ok);
-              if ((d != 0 || ok) && !isnan(d)) {
+              if ((d != 0 || ok) && !IS_NAN(d)) {
                 d += screen.x();
                 if (d < screen.x() || d > screen.right())
 		  d = screen.x(); // only safe choice until size is determined
@@ -1735,7 +1727,7 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
             } else if (key == "top" || key == "screeny") {
               bool ok;
               double d = val.toDouble(&ok);
-              if ((d != 0 || ok) && !isnan(d)) {
+              if ((d != 0 || ok) && !IS_NAN(d)) {
                 d += screen.y();
                 if (d < screen.y() || d > screen.bottom())
 		  d = screen.y(); // only safe choice until size is determined
@@ -1747,7 +1739,7 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
             } else if (key == "height") {
               bool ok;
               double d = val.toDouble(&ok);
-              if ((d != 0 || ok) && !isnan(d)) {
+              if ((d != 0 || ok) && !IS_NAN(d)) {
 #if !APPLE_CHANGES
                 d += 2*qApp->style().pixelMetric( QStyle::PM_DefaultFrameWidth ) + 2;
 #endif
@@ -1763,7 +1755,7 @@ Value WindowFunc::tryCall(ExecState *exec, Object &thisObj, const List &args)
             } else if (key == "width") {
               bool ok;
               double d = val.toDouble(&ok);
-              if ((d != 0 || ok) && !isnan(d)) {
+              if ((d != 0 || ok) && !IS_NAN(d)) {
 #if !APPLE_CHANGES
                 d += 2*qApp->style().pixelMetric( QStyle::PM_DefaultFrameWidth ) + 2;
 #endif
