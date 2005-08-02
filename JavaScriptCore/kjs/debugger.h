2f5058d578572b57943531ee407516d9b0c7bf00
ggaren <ggaren@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2005-08-02 23:06:42 +0000

    - minor tweak to last checkin
            * kjs/debugger.h: restored a comment I shouldn't have deleted
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Safari-2-0-branch@10020 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/debugger.h
+++ JavaScriptCore/kjs/debugger.h
@@ -102,6 +102,7 @@ namespace KJS {
      * @param sourceId The ID of the source code (corresponds to the
      * sourceId supplied in other functions such as @ref atStatement()
      * @param sourceURL Where the source code that was parsed came from 
+     * @param source The source code that was parsed
      * @param errorLine The line number at which parsing encountered an
      * error, or -1 if the source code was valid and parsed succesfully
      * @return true if execution should be continue, false if it should
