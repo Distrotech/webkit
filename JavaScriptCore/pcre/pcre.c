865e6b10600f729ef795f326a9a609f240c146d1
ggaren <ggaren@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2005-11-04 23:26:49 +0000

    Reviewed by darin.
            - Fixed <rdar://problem/4317701> *SecUpd: Tiger* PCRE heap overflow
              in Safari JavaScriptCore
    
            * pcre/pcre.c:
            (read_repeat_counts): We now return an error for negative repeat counts
            in regular expressions. This matches Firefox exactly, and Win IE by
            effect (Win IE doesn't return an error, but it does refuse to match
            anything).
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Ti-2005-009-branch@11066 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/pcre/pcre.c
+++ JavaScriptCore/pcre/pcre.c
@@ -771,6 +771,11 @@ int min = 0;
 int max = -1;
 
 while ((cd->ctypes[*p] & ctype_digit) != 0) min = min * 10 + *p++ - '0';
+if (min < 0 || min > 65535)
+  {
+  *errorptr = ERR5;
+  return p;
+  }
 
 if (*p == '}') max = min; else
   {
@@ -778,6 +783,11 @@ if (*p == '}') max = min; else
     {
     max = 0;
     while((cd->ctypes[*p] & ctype_digit) != 0) max = max * 10 + *p++ - '0';
+    if (max < 0 || max > 65535)
+      {
+      *errorptr = ERR5;
+      return p;
+      }
     if (max < min)
       {
       *errorptr = ERR4;
@@ -786,16 +796,12 @@ if (*p == '}') max = min; else
     }
   }
 
-/* Do paranoid checks, then fill in the required variables, and pass back the
-pointer to the terminating '}'. */
+/* fill in the required variables, and pass back the pointer to the 
+terminating '}'. */
 
-if (min > 65535 || max > 65535)
-  *errorptr = ERR5;
-else
-  {
-  *minp = min;
-  *maxp = max;
-  }
+*minp = min;
+*maxp = max;
+
 return p;
 }
 
