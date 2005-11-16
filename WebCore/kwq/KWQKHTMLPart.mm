572c2ef2d39f1ab166c9ca037d3f7f366f0acaed
thatcher <thatcher@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2005-11-16 00:24:40 +0000

    Merged fix from TOT to Safari-1-3-branch
        2005-11-13  Vicki Murley  <vicki@apple.com>
    
            Reviewed by Maciej.
    
            Fixed <rdar://problem/4316201> REGRESSION: cursor and text in status bar is not updated when mousing over links in a frame (5509)
    
            * kwq/KWQKHTMLPart.mm:
            (KWQKHTMLPart::passSubframeEventToSubframe): add a case to handle NSMouseMoved, to fix the problem with the hand cursor
            (KWQKHTMLPart::mouseMoved): return if we're already sending an event to the subview
            * kwq/WebCoreBridge.mm:
            (-[WebCoreBridge elementAtPoint:]): if we're over a frame, return the element within the frame, to fix the problem with
            status bar text
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Safari-1-3-branch@11208 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/kwq/KWQKHTMLPart.mm
+++ WebCore/kwq/KWQKHTMLPart.mm
@@ -2086,7 +2086,6 @@ bool KWQKHTMLPart::passWidgetMouseDownEventToWidget(QWidget* widget)
     if (!wasDeferringTimers) {
         QObject::setDefersTimers(true);
     }
-
     ASSERT(!_sendingEventToSubview);
     _sendingEventToSubview = true;
     [view mouseDown:_currentEvent];
@@ -2512,6 +2511,23 @@ bool KWQKHTMLPart::passSubframeEventToSubframe(NodeImpl::MouseEvent &event)
     KWQ_BLOCK_EXCEPTIONS;
 
     switch ([_currentEvent type]) {
+        case NSMouseMoved: {
+            NodeImpl *node = event.innerNode.handle();
+            if (!node)
+                return false;
+            RenderObject *renderer = node->renderer();
+            if (!renderer || !renderer->isWidget())
+                return false;
+            QWidget *widget = static_cast<RenderWidget *>(renderer)->widget();
+            if (!widget || !widget->inherits("KHTMLView"))
+                return false;
+            KHTMLPart *subframePart = static_cast<KHTMLView *>(widget)->part();
+            if (!subframePart)
+                return false;
+            [KWQ(subframePart)->bridge() mouseMoved:_currentEvent];
+            return true;
+        }
+        
     	case NSLeftMouseDown: {
             NodeImpl *node = event.innerNode.handle();
             if (!node) {
@@ -2722,7 +2738,7 @@ void KWQKHTMLPart::mouseMoved(NSEvent *event)
     KHTMLView *v = d->m_view;
     // Reject a mouse moved if the button is down - screws up tracking during autoscroll
     // These happen because WebKit sometimes has to fake up moved events.
-    if (!v || d->m_bMousePressed) {
+    if (!v || d->m_bMousePressed || _sendingEventToSubview) {
         return;
     }
     
