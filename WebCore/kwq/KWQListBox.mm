dbe2c156212b4fe3caf6241958d6696e2ede1aa7
thatcher <thatcher@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2005-11-17 18:32:16 +0000

    Merged fix from TOT to Safari-1-3-branch
        2005-11-15  Adele Peterson  <adele@apple.com>
    
            Reviewed by Darin.
    
            - fix for <rdar://problem/4341948> REGRESSION(TOT/GLENDALE): Safari won't tab between input/textarea elements if a select element is present
    
            * kwq/KWQComboBox.mm: (-[KWQPopUpButton canBecomeKeyView]): removed check for tabsToAllControls() since we now always allow tabbing to select elements
            * kwq/KWQListBox.mm: (-[KWQTableView canBecomeKeyView]): ditto.
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Safari-1-3-branch@11231 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/kwq/KWQListBox.mm
+++ WebCore/kwq/KWQListBox.mm
@@ -498,9 +498,6 @@ void QListBox::clearCachedTextRenderers()
 {
     // Simplified method from NSView; overridden to replace NSView's way of checking
     // for full keyboard access with ours.
-    if (!_box || !KWQKHTMLPart::partForWidget(_box)->tabsToAllControls()) {
-        return NO;
-    }
     return ([self window] != nil) && ![self isHiddenOrHasHiddenAncestor] && [self acceptsFirstResponder];
 }
 
