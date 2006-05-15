f39d409d085309655adb106f17601a4c512d31ce
thatcher <thatcher@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2006-05-15 20:25:48 +0000

    Tag for WebKit 418.4 (part 2 of 2.)
    git-svn-id: http://svn.webkit.org/repository/webkit/tags/WebKit-418.4@14386 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/Misc.subproj/WebNSWindowExtras.h
@@ -0,0 +1,24 @@
+//
+//  WebNSWindowExtras.h
+//  WebKit
+//
+//  Created by Darin Adler on Tue Oct 22 2002.
+//  Copyright (c) 2002 Apple Computer, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSWindow (WebExtras)
+// Throttles the NSWindow autodisplay mechanism to a maximum of 60 frames per second.
+// This ensures that dynamically updated web content does not cause the window to display
+// too often, which can cause performance problems with CoreGraphics' coalesced updates
+// feature.  See <http://developer.apple.com/technotes/tn2005/tn2133.html>.
++ (void)_webkit_enableWindowDisplayThrottle;
+
+// Disables NSWindow display throttling.  Any windows with pending displays will be displayed
+// immediately when window throttling is disabled.
++ (void)_webkit_disableWindowDisplayThrottle;
+
+// centers "visually", putting 1/3 of the remaining space above, and 2/3 below
+- (void)centerOverMainWindow; 
+@end
---
+++ WebKit/WebView.subproj/WebViewInternal.h
@@ -0,0 +1,110 @@
+// This header contains WebView declarations that can be used anywhere in the Web Kit, but are neither SPI nor API.
+
+#import <WebKit/WebViewPrivate.h>
+
+@class DOMCSSStyleDeclaration;
+@class WebBackForwardList;
+
+@protocol WebDocumentDragging;
+
+@interface WebViewPrivate : NSObject
+{
+@public
+    WebFrame *mainFrame;
+    
+    id UIDelegate;
+    id UIDelegateForwarder;
+    id resourceProgressDelegate;
+    id resourceProgressDelegateForwarder;
+    id downloadDelegate;
+    id policyDelegate;
+    id policyDelegateForwarder;
+    id frameLoadDelegate;
+    id frameLoadDelegateForwarder;
+    id <WebFormDelegate> formDelegate;
+    id editingDelegate;
+    id editingDelegateForwarder;
+    id scriptDebugDelegate;
+    id scriptDebugDelegateForwarder;
+    
+    WebBackForwardList *backForwardList;
+    BOOL useBackForwardList;
+    
+    float textSizeMultiplier;
+
+    NSString *applicationNameForUserAgent;
+    NSString *userAgent;
+    BOOL userAgentOverridden;
+    
+    BOOL defersCallbacks;
+
+    NSString *setName;
+
+    WebPreferences *preferences;
+    WebCoreSettings *settings;
+        
+    BOOL lastElementWasNonNil;
+
+    NSWindow *hostWindow;
+
+    int programmaticFocusCount;
+    
+    WebResourceDelegateImplementationCache resourceLoadDelegateImplementations;
+
+    long long totalPageAndResourceBytesToLoad;
+    long long totalBytesReceived;
+    double progressValue;
+    double lastNotifiedProgressValue;
+    double lastNotifiedProgressTime;
+    double progressNotificationInterval;
+    double progressNotificationTimeInterval;
+    BOOL finalProgressChangedSent;
+    WebFrame *orginatingProgressFrame;
+    
+    int numProgressTrackedFrames;
+    NSMutableDictionary *progressItems;
+    
+    void *observationInfo;
+    
+    BOOL drawsBackground;
+    BOOL editable;
+    BOOL initiatedDrag;
+        
+    NSString *mediaStyle;
+    
+    NSView <WebDocumentDragging> *draggingDocumentView;
+    unsigned int dragDestinationActionMask;
+    WebBridge *dragCaretBridge;
+    
+    BOOL hasSpellCheckerDocumentTag;
+    int spellCheckerDocumentTag;
+
+    BOOL continuousSpellCheckingEnabled;
+    BOOL continuousGrammarCheckingEnabled;
+    BOOL smartInsertDeleteEnabled;
+    
+    BOOL dashboardBehaviorAlwaysSendMouseEventsToAllWindows;
+    BOOL dashboardBehaviorAlwaysSendActiveNullEventsToPlugIns;
+    BOOL dashboardBehaviorAlwaysAcceptsFirstMouse;
+    BOOL dashboardBehaviorAllowWheelScrolling;
+    
+    BOOL shouldUseFontSmoothing;
+    BOOL selectWordBeforeMenuEvent;
+}
+@end
+
+@interface WebView (WebInternal)
+- (WebFrame *)_frameForCurrentSelection;
+- (WebBridge *)_bridgeForCurrentSelection;
+- (BOOL)_isLoading;
+
+- (WebFrameView *)_frameViewAtWindowPoint:(NSPoint)point;
+- (WebBridge *)_bridgeAtPoint:(NSPoint)point;
+@end;
+
+@interface WebView (WebViewEditingExtras)
+- (BOOL)_interceptEditingKeyEvent:(NSEvent *)event;
+- (BOOL)_shouldBeginEditingInDOMRange:(DOMRange *)range;
+- (BOOL)_shouldEndEditingInDOMRange:(DOMRange *)range;
+- (BOOL)_canPaste;
+@end
