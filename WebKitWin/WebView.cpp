aec82e3c329b6196efa134b8aa488e24e085669b
bdakin <bdakin@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2007-02-22 23:59:33 +0000

    Reviewed by Adam.
            Fix for http://bugs.webkit.org/show_bug.cgi?id=12399 REGRESSION: 
            Unable to prevent default context menu from appearing. <rdar://
            problem/5017416>
    
            * WebView.cpp:
            (WebView::handleContextMenuEvent): Clear the controller's context 
            menu before propagating a new context menu event through the DOM.
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/WindowsMerge@22986 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitWin/WebView.cpp
+++ WebKitWin/WebView.cpp
@@ -537,6 +537,13 @@ bool WebView::handleContextMenuEvent(WPARAM wParam, LPARAM lParam)
 
     lParam = MAKELPARAM(coords.x, coords.y);
 
+    // The contextMenuController() holds onto the last context menu that was popped up on the
+    // page until a new one is created. We need to clear this menu before propagating the event
+    // through the DOM so that we can detect if we create a new menu for this event, since we
+    // won't create a new menu if the DOM swallows the event and the defaultEventHandler does
+    // not run.
+    m_page->contextMenuController()->clearContextMenu();
+
     PlatformMouseEvent mouseEvent(m_viewWindow, WM_RBUTTONUP, wParam, lParam);
     bool handledEvent = m_page->mainFrame()->eventHandler()->sendContextMenuEvent(mouseEvent);
     if (!handledEvent)
