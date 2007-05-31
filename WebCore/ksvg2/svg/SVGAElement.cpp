cfe00b4da3f0e556536fa5aed58f7760f36e20a4
eseidel <eseidel@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2007-05-31 20:53:44 +0000

    2007-05-31  Eric Seidel  <eric@webkit.org>
            Reviewed by Niko.
    
            Links are activated based on mouse down events instead of click events
            http://bugs.webkit.org/show_bug.cgi?id=12570
            
            Test: svg/custom/prevent-default.svg
    
            * html/HTMLAnchorElement.cpp: use MiddleButton and RightButton instead of 1 and 2
            (WebCore::HTMLAnchorElement::defaultEventHandler):
            * ksvg2/svg/SVGAElement.cpp:
            (WebCore::SVGAElement::defaultEventHandler): make it function more like the HTML side
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/feature-branch@21920 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/ksvg2/svg/SVGAElement.cpp
+++ WebCore/ksvg2/svg/SVGAElement.cpp
@@ -1,6 +1,7 @@
 /*
     Copyright (C) 2004, 2005 Nikolas Zimmermann <wildfox@kde.org>
                   2004, 2005, 2007 Rob Buis <buis@kde.org>
+                  2007 Eric Seidel <eric@webkit.org>
 
     This file is part of the KDE project
 
@@ -31,6 +32,7 @@
 #include "EventNames.h"
 #include "Frame.h"
 #include "FrameLoader.h"
+#include "KeyboardEvent.h"
 #include "MouseEvent.h"
 #include "PlatformMouseEvent.h"
 #include "RenderSVGInline.h"
@@ -41,6 +43,8 @@
 #include "csshelper.h"
 
 namespace WebCore {
+
+using namespace EventNames;
 
 SVGAElement::SVGAElement(const QualifiedName& tagName, Document *doc)
     : SVGStyledTransformableElement(tagName, doc)
@@ -95,14 +99,30 @@ RenderObject* SVGAElement::createRenderer(RenderArena* arena, RenderStyle* style
 void SVGAElement::defaultEventHandler(Event* evt)
 {
     // TODO : should use CLICK instead
-    if ((evt->type() == EventNames::mouseupEvent && evt->isMouseEvent() && static_cast<MouseEvent*>(evt)->button() != RightButton && m_isLink)) {
-        MouseEvent* e = static_cast<MouseEvent*>(evt);
-
+    if (m_isLink && (evt->type() == clickEvent || (evt->type() == keydownEvent && m_focused))) {
+        MouseEvent* e = 0;
+        if (evt->type() == clickEvent && evt->isMouseEvent())
+            e = static_cast<MouseEvent*>(evt);
+        
+        KeyboardEvent* k = 0;
+        if (evt->type() == keydownEvent && evt->isKeyboardEvent())
+            k = static_cast<KeyboardEvent*>(evt);
+        
         if (e && e->button() == RightButton) {
             SVGStyledTransformableElement::defaultEventHandler(evt);
             return;
         }
-
+        
+        if (k) {
+            if (k->keyIdentifier() != "Enter") {
+                SVGStyledTransformableElement::defaultEventHandler(evt);
+                return;
+            }
+            evt->setDefaultHandled();
+            dispatchSimulatedClick(evt);
+            return;
+        }
+        
         String target = getAttribute(SVGNames::targetAttr);
         String xlinktarget = getAttribute(XLinkNames::showAttr);
         if (e && e->button() == MiddleButton)
