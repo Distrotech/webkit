f9370e53ddbd4dee58296ae3982d1ab74fbaa28d
thatcher <thatcher@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2007-10-10 23:30:05 +0000

    Moving old branch to branches/old.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/old@26333 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebFoundation-SDK-branch/WebKit/Misc.subproj/WebFileDatabase.h
@@ -0,0 +1,23 @@
+/*	WebFileDatabase.h
+	Copyright 2002-2003, Apple, Inc. All rights reserved.
+*/
+
+#import <Foundation/Foundation.h>
+
+#import "WebDatabase.h"
+
+@interface WebFileDatabase : WebDatabase 
+{
+    struct WebLRUFileList *lru;
+    NSMutableArray *ops;
+    NSMutableDictionary *setCache;
+    NSMutableSet *removeCache;
+    NSTimer *timer;
+    NSTimeInterval touch;
+    NSRecursiveLock *mutex;
+}
+
+-(void)performSetObject:(id)object forKey:(id)key;
+-(void)performRemoveObjectForKey:(id)key;
+
+@end
