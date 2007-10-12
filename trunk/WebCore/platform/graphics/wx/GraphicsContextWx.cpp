04e04bd382307e89f7d1528d86b77e8ebe11cdfb
kevino <kevino@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2007-10-12 02:33:49 +0000

    64-bit compilation fix.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/wx-port-alpha@26379 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- trunk/WebCore/platform/graphics/wx/GraphicsContextWx.cpp
+++ trunk/WebCore/platform/graphics/wx/GraphicsContextWx.cpp
@@ -465,17 +465,17 @@ FloatRect GraphicsContext::roundToDevicePixels(const FloatRect& frect)
 {
     FloatRect result;
 
-    double x =frect.x();
-    double y = frect.y();
+    wxCoord x = (wxCoord)frect.x();
+    wxCoord y = (wxCoord)frect.y();
 
-    m_data->context->LogicalToDeviceX((wxCoord)&x);
-    m_data->context->LogicalToDeviceY((wxCoord)&y);
+    x = m_data->context->LogicalToDeviceX(x);
+    y = m_data->context->LogicalToDeviceY(y);
     result.setX((float)x);
     result.setY((float)y);
-    x = frect.width();
-    y = frect.height();
-    m_data->context->LogicalToDeviceXRel((wxCoord)&x);
-    m_data->context->LogicalToDeviceYRel((wxCoord)&y);
+    x = (wxCoord)frect.width();
+    y = (wxCoord)frect.height();
+    x = m_data->context->LogicalToDeviceXRel(x);
+    y = m_data->context->LogicalToDeviceYRel(y);
     result.setWidth((float)x);
     result.setHeight((float)y);
     return result; 
