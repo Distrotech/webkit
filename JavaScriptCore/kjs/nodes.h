c071e12236f965db2e0f0bdb52ce5a150d7fcc95
oliver@apple.com <oliver@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-03-27 20:58:24 +0000

    Bug 18142: squirrelfish needs to support dynamic scoping/with <http://bugs.webkit.org/show_bug.cgi?id=18142>
    Reviewed by Maciej
    
    Add support for dynamic scoping and add code to handle 'with'
    statements.
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/squirrelfish@31373 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/nodes.h
+++ JavaScriptCore/kjs/nodes.h
@@ -2745,6 +2745,7 @@ namespace KJS {
         {
         }
 
+        virtual RegisterID* emitCode(CodeGenerator&, RegisterID* = 0) KJS_FAST_CALL;
         virtual void optimizeVariableAccess(ExecState*, const SymbolTable&, const LocalStorage&, NodeStack&) KJS_FAST_CALL;
         virtual JSValue* execute(ExecState*) KJS_FAST_CALL;
         virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
