583c140fe330c40fb6dac31ec05d532ebf5a69ce
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-05-13 05:53:19 +0000

    2008-03-31  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Maciej.
    
            Add support for FunctionCallValueNode.
    
            * kjs/nodes.cpp:
            (KJS::FunctionCallValueNode::emitCode):
            * kjs/nodes.h:
    
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/squirrelfish@33111 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/nodes.h
+++ JavaScriptCore/kjs/nodes.h
@@ -809,6 +809,7 @@ namespace KJS {
         {
         }
 
+        virtual RegisterID* emitCode(CodeGenerator&, RegisterID* = 0) KJS_FAST_CALL;
         virtual void optimizeVariableAccess(ExecState*, const SymbolTable&, const LocalStorage&, NodeStack&) KJS_FAST_CALL;
         virtual JSValue* evaluate(ExecState*) KJS_FAST_CALL;
         virtual void streamTo(SourceStream&) const KJS_FAST_CALL;
