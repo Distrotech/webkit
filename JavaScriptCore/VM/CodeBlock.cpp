989c724dc5d9f6a16749b6c342f8ad4dc53e9574
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-05-20 06:56:09 +0000

    JavaScriptCore:
    2008-05-08  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Oliver Hunt.
    
            Fixed failure in fast/canvas/canvas-pattern-behaviour.html.
    
            SunSpider reports a small speedup. Not sure what that's about.
    
            * VM/CodeBlock.cpp:
            (KJS::CodeBlock::dump): Fixed op_call_eval to dump as "op_call_eval".
            This helped me while debugging.
    
            * VM/Machine.cpp:
            (KJS::Machine::unwindCallFrame): When looking for an activation to tear
            off, don't use the scope chain. Inside eval, the scope chain doesn't
            belong to us; it belongs to our calling function.
    
            Also, don't use the needsFullScopeChain flag to decide whether to tear
            off the activation. "function.arguments" can create an activation
            for a function whose needsFullScopeChain flag is set to false.
    
    LayoutTests:
    
    2008-05-08  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Oliver Hunt.
    
            Added specific test cases for the root cause of squirrelfish's failure
            in fast/canvas/canvas-pattern-behaviour.html.
    
            * fast/js/exception-thrown-from-eval-inside-closure-expected.txt: Added.
            * fast/js/exception-thrown-from-eval-inside-closure.html: Added.
            * fast/js/exception-thrown-from-function-with-lazy-activation-expected.txt: Added.
            * fast/js/exception-thrown-from-function-with-lazy-activation.html: Added.
    
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/squirrelfish@33879 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/VM/CodeBlock.cpp
+++ JavaScriptCore/VM/CodeBlock.cpp
@@ -436,7 +436,7 @@ void CodeBlock::dump(ExecState* exec, const Vector<Instruction>::const_iterator&
             int r2 = (++it)->u.operand;
             int tempCount = (++it)->u.operand;
             int argCount = (++it)->u.operand;
-            printf("[%4d] eval\t\t %s, %s, %s, %d, %d\n", location, registerName(r0).c_str(), registerName(r1).c_str(), registerName(r2).c_str(), tempCount, argCount);
+            printf("[%4d] call_eval\t\t %s, %s, %s, %d, %d\n", location, registerName(r0).c_str(), registerName(r1).c_str(), registerName(r2).c_str(), tempCount, argCount);
             break;
         }
         case op_ret: {
