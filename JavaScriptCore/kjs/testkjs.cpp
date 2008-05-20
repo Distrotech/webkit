9f97063448177c6e2931fbe2f10815e1d89d3f44
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-05-20 06:47:06 +0000

    2008-04-07  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Oliver Hunt.
    
            Restored dumping of generated code as a command-line switch:
            run-testkjs -d will do it.
    
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/squirrelfish@33688 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/testkjs.cpp
+++ JavaScriptCore/kjs/testkjs.cpp
@@ -22,6 +22,7 @@
 
 #include "config.h"
 
+#include "CodeGenerator.h"
 #include "JSGlobalObject.h"
 #include "JSLock.h"
 #include "Parser.h"
@@ -273,10 +274,13 @@ static bool prettyPrintScript(const UString& fileName, const Vector<char>& scrip
     return true;
 }
 
-static bool runWithScripts(const Vector<UString>& fileNames, Vector<UString>& arguments, bool prettyPrint)
+static bool runWithScripts(const Vector<UString>& fileNames, Vector<UString>& arguments, bool prettyPrint, bool dump)
 {
     GlobalObject* globalObject = new GlobalObject(arguments);
     Vector<char> script;
+
+    if (dump)
+        CodeGenerator::setDumpsGeneratedCode(true);
 
     bool success = true;
 
@@ -302,7 +306,7 @@ static void printUsageStatement()
     exit(-1);
 }
 
-static void parseArguments(int argc, char** argv, Vector<UString>& fileNames, Vector<UString>& arguments, bool& prettyPrint)
+static void parseArguments(int argc, char** argv, Vector<UString>& fileNames, Vector<UString>& arguments, bool& prettyPrint, bool& dump)
 {
     if (argc < 3)
         printUsageStatement();
@@ -318,6 +322,10 @@ static void parseArguments(int argc, char** argv, Vector<UString>& fileNames, Ve
         }
         if (strcmp(arg, "-p") == 0) {
             prettyPrint = true;
+            continue;
+        }
+        if (strcmp(arg, "-d") == 0) {
+            dump = true;
             continue;
         }
         if (strcmp(arg, "--") == 0) {
@@ -338,11 +346,12 @@ int kjsmain(int argc, char** argv)
     JSLock lock;
 
     bool prettyPrint = false;
+    bool dump = false;
     Vector<UString> fileNames;
     Vector<UString> arguments;
-    parseArguments(argc, argv, fileNames, arguments, prettyPrint);
+    parseArguments(argc, argv, fileNames, arguments, prettyPrint, dump);
 
-    bool success = runWithScripts(fileNames, arguments, prettyPrint);
+    bool success = runWithScripts(fileNames, arguments, prettyPrint, dump);
 
 #ifndef NDEBUG
     Collector::collect();
