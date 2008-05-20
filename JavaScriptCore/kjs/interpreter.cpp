69ec3425ba36e5d441080c9ee8d2454f73a31791
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-05-20 06:49:10 +0000

    2008-04-16  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Oliver Hunt.
    
            Made "this" work in program code / global scope.
    
            The machine can initialize "this" prior to execution because it knows
            that, for program code, "this" is always stored in lr1.
    
            * VM/Machine.cpp:
            (KJS::Machine::execute):
            * VM/Machine.h:
            (KJS::Machine::):
            * kjs/interpreter.cpp:
            (KJS::Interpreter::evaluate):
    
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/squirrelfish@33736 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/interpreter.cpp
+++ JavaScriptCore/kjs/interpreter.cpp
@@ -59,7 +59,7 @@ Completion Interpreter::evaluate(ExecState* exec, const UString& sourceURL, int 
     return evaluate(exec, sourceURL, startingLineNumber, code.data(), code.size(), thisV);
 }
 
-Completion Interpreter::evaluate(ExecState* exec, const UString& sourceURL, int startingLineNumber, const UChar* code, int codeLength, JSValue*)
+Completion Interpreter::evaluate(ExecState* exec, const UString& sourceURL, int startingLineNumber, const UChar* code, int codeLength, JSValue* thisValue)
 {
     JSLock lock;
     
@@ -78,8 +78,10 @@ Completion Interpreter::evaluate(ExecState* exec, const UString& sourceURL, int 
     if (!programNode)
         return Completion(Throw, Error::create(exec, SyntaxError, errMsg, errLine, sourceId, sourceURL));
 
+    JSObject* thisObj = (!thisValue || thisValue->isUndefinedOrNull()) ? exec->dynamicGlobalObject() : thisValue->toObject(exec);
+
     JSValue* exception = 0;
-    JSValue* result = machine().execute(programNode.get(), exec, &exec->dynamicGlobalObject()->registerFileStack(), &exec->scopeChain(), &exception);
+    JSValue* result = machine().execute(programNode.get(), exec, thisObj, &exec->dynamicGlobalObject()->registerFileStack(), &exec->scopeChain(), &exception);
 
 #if JAVASCRIPT_PROFILING
     Profiler::profiler()->didExecute(exec, sourceURL, startingLineNumber);
