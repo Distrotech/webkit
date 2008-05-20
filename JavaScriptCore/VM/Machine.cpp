b2b1f6f23365bf980f7f72b9732afc9680f3160e
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-05-20 06:51:42 +0000

    2008-04-23  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Oliver.
    
            - save and restore callFrame
    
            * VM/Machine.cpp:
            (KJS::slideRegisterWindowForCall):
            (KJS::Machine::execute):
            (KJS::Machine::privateExecute):
            * kjs/testkjs.cpp:
            (main):
    
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/squirrelfish@33794 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/VM/Machine.cpp
+++ JavaScriptCore/VM/Machine.cpp
@@ -564,7 +564,8 @@ JSValue* Machine::execute(FunctionBodyNode* functionBodyNode, const List& args, 
     
     Register** registerBase = registerFile->basePointer();
     int registerOffset = oldSize;
-    Register* callFrame = (*registerBase) + registerOffset;
+    int callFrameOffset = registerOffset;
+    Register* callFrame = (*registerBase) + callFrameOffset;
     
     // put args in place, including "this"
     Register* dst = callFrame + CallFrameHeaderSize;
@@ -579,6 +580,7 @@ JSValue* Machine::execute(FunctionBodyNode* functionBodyNode, const List& args, 
 
     CodeBlock* newCodeBlock = &functionBodyNode->code(scopeChain);
     Register* r = slideRegisterWindowForCall(newCodeBlock, registerFile, registerBase, registerOffset, argv, argc);
+    callFrame = (*registerBase) + callFrameOffset; // registerBase may have moved, recompute callFrame
     scopeChain = scopeChainForCall(newCodeBlock, scopeChain, functionBodyNode, callFrame, registerBase, r);            
 
     JSValue* result = privateExecute(Normal, exec, registerFile, r, scopeChain, newCodeBlock, exception);
@@ -1314,6 +1316,7 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
         if (callType == CallTypeJS) {
             int registerOffset = r - (*registerBase);
             Register* callFrame = r + argv - CallFrameHeaderSize;
+            int callFrameOffset = registerOffset + argv - CallFrameHeaderSize;
 
             r[argv].u.jsValue = r2 == missingSymbolMarker() ? exec->globalThisValue() : r[r2].u.jsValue; // "this" value
             initializeCallFrame(callFrame, codeBlock, vPC, scopeChain, registerOffset, r0, argv, 0);
@@ -1323,6 +1326,7 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
 
             codeBlock = &functionBodyNode->code(callDataScopeChain);
             r = slideRegisterWindowForCall(codeBlock, registerFile, registerBase, registerOffset, argv, argc);
+            callFrame = (*registerBase) + callFrameOffset; // registerBase may have moved, recompute callFrame
             scopeChain = scopeChainForCall(codeBlock, callDataScopeChain, functionBodyNode, callFrame, registerBase, r);            
             k = codeBlock->jsValues.data();
             vPC = codeBlock->instructions.begin();
@@ -1406,6 +1410,7 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
         if (constructType == ConstructTypeJS) {
             int registerOffset = r - (*registerBase);
             Register* callFrame = r + argv - CallFrameHeaderSize;
+            int callFrameOffset = registerOffset + argv - CallFrameHeaderSize;
 
             JSObject* prototype;
             JSValue* p = constructor->get(exec, exec->propertyNames().prototype);
@@ -1423,6 +1428,7 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, ExecState* exec, RegisterFi
 
             codeBlock = &functionBodyNode->code(callDataScopeChain);
             r = slideRegisterWindowForCall(codeBlock, registerFile, registerBase, registerOffset, argv, argc);
+            callFrame = (*registerBase) + callFrameOffset; // registerBase may have moved, recompute callFrame
             scopeChain = scopeChainForCall(codeBlock, callDataScopeChain, functionBodyNode, callFrame, registerBase, r);            
             k = codeBlock->jsValues.data();
             vPC = codeBlock->instructions.begin();
